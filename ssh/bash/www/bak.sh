0bcfffb6a9715f093df090bd95a007fb304588f2
florianthepro <florianthepro@icloud.com>
2026-07-27 09:49:49 +0200

    Update and rename bak.md to bak.sh

--- ssh/bash/www/bak.sh
+++ ssh/bash/www/bak.sh
@@ -1,3 +1,1 @@
-```
 id=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) && zip -rq "/tmp/${id}.zip" /var/www/html && ip=$(hostname -I | awk '{print $1}') && clear && echo "<id.zip> ${id}.zip" && echo && echo "Windows (PowerShell): scp ${USER}@${ip}:/tmp/${id}.zip ." && echo "Linux: scp ${USER}@${ip}:/tmp/${id}.zip ." && echo "macOS: scp ${USER}@${ip}:/tmp/${id}.zip ."
-```
